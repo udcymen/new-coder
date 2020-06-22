e72a412b52e8ea2dcea53fce8ada323860766b8f
Cong <samuelmc1015@gmail.com>
2020-06-22 18:04:42 -0400

    Revert "rename to new-coder"
    This reverts commit a6a2497da197aa676071d60a90b90c6129a80bd7.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# new-coder
+# NewCoder
--- backend/src/main/java/com/newcoder/NewCoderApplication.java
+++ backend/src/main/java/com/newcoder/NewCoderApplication.java
@@ -2,8 +2,10 @@ package com.newcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NewCoderApplication {
 
 	public static void main(String[] args) {
