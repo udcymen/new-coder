c7887f819306cf3e0c465718efeb8d1253a5ea2a
Cong Meng <samuelmc1015@gmail.com>
2020-06-22 01:19:48 -0400

    docker with spring boot and postgres

--- SpringBoot/src/test/java/com/newcoder/NewCoderApplicationTests.java
+++ SpringBoot/src/test/java/com/newcoder/NewCoderApplicationTests.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewcoderApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class NewCoderApplicationTests {
 
 }
