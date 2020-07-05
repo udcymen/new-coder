0b962607ea860d637f4d7ee3edbeec890d9be193
Cong <samuelmc1015@gmail.com>
2020-07-04 20:51:36 -0400

    add serialVersionUID

--- backend/src/main/java/com/newcoder/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/newcoder/exception/ResourceNotFoundException.java
@@ -1,7 +1,12 @@
 package com.newcoder.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 90026614827330135L;
+
+	public ResourceNotFoundException(String message) {
         super(message);
     }
 
